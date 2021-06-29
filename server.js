ac42231a54769c4a6e23738da0a6ab9b60e67421
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-29 16:05:10 -0400

    updated

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const path = require ('path');
 const app = express ();
 
 app.use(express.json())
-
-
-const port = process.env.PORT || 4545;
+//rollbar
+// const Rollbar = require('rollbar')
+// const rollbar = new Rollbar({
+//   accessToken: '4656e99901f74303ab58e5ab004444ed',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
+app.get('/',function(req,res){
+    //rollbar.log('Hello World')
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+const port = process.env.PORT || 4444;
 app.listen(port,function(){ 
     console.log(`server running on ${port}`)
 })
