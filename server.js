113665f659648badbb476d1890e987ec46acb7b1
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-29 16:41:34 -0400

    adde rollbar

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');  // This function starts your .js requiring express to be used
 const path = require('path');  //talks with our html file 
 const app = express();  //This is the function that puts express to use
-
+const  Rollbar = require("rollbar");
+var   rollbar = new Rollbar({
+  accessToken: '9aa4246902434637a0cd07104d4531b7',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 app.use(express.json());
-
+rollbar.log('this is tarceability')
 app.get('/', function(req, res) {
+
     res.sendFile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
 //path.join - join the location of index.html to the current directory
 }); //__dirname - always the first argument of the directory at this location (monitoring-interactive)
