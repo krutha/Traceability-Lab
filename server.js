fccc514b99078f4ca40cf50f68c575d939c55a26
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-29 17:35:21 -0400

    catch test

--- server.js
+++ server.js
@@ -9,16 +9,50 @@ var   rollbar = new Rollbar({
 });
 app.use(express.json());
 //rollbar.log('this is tarceability')
-
 app.get('/', function(req, res) {
     // rollbar.log('hello world');
     //rollbar.error('link broken');//
     rollbar.info('html file served successfully');
     rollbar.critical("Crash the server");
     res.sendFile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
-   // res.sendFile(path.join(__dirname, '/public/broken.html'))
-//path.join - join the location of index.html to the current directory
-}); //__dirname - always the first argument of the directory at this location (monitoring-interactive)
+    res.sendFile(path.join(__dirname, '/public/broken.html'))//path.join - join the location of index.html to the current directory
+ }); //__dirname - always the first argument of the directory at this location (monitoring-interactive)
+let students = [] // we'll hold any students added here
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
+})
+
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex((studentName) => { // check if student name exists already
+        return studentName === name
+    })
+
+    console.log(index)
+
+    try { // using a "try catch" block will handle any generic 500 errors (not necessary, but a good addition)
+        if (index === -1 && name !== '') {
+            // we'll send responses to the user based upon whether or not they gave us a valid user to add
+            // also we'll send information to rollbar so we can keep track of the activity that's happening
+            students.push(name)
+            rollbar.log('student added successfully', {author: 'riley', type: 'manual'})
+            res.status(200).send(students)
+        } else if (name === '') {
+            rollbar.error('no name given')
+            res.status(400).send('must provide a name')
+        } else {
+            rollbar.error('student already exists')
+            res.status(400).send('that student already exists')
+        }
+    } catch (err) {
+        rollbar.error(err)
+    }
+})
+
 
 const port = process.env.PORT || 4413;
 app.listen(port, function() {
