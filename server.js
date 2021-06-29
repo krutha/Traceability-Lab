3930403778fbbb9d4041e7fbbbce800d5f8788e0
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-29 16:21:39 -0400

    test3

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express');  // This function starts your .js requiring express to be used
+const path = require('path');  //talks with our html file 
+const app = express();  //This is the function that puts express to use
 
-app.use(express.json())
-//rollbar
-// const Rollbar = require('rollbar')
-// const rollbar = new Rollbar({
-//   accessToken: '4656e99901f74303ab58e5ab004444ed',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
-app.get('/',function(req,res){
-    //rollbar.log('Hello World')
-    res.sendFile(path.join(__dirname,'/public/index.html'))
-})
-const port = process.env.PORT || 4444;
-app.listen(port, function(){ 
-    console.log(`server running on ${port}`)
-})
+app.use(express.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
+//path.join - join the location of index.html to the current directory
+}); //__dirname - always the first argument of the directory at this location (monitoring-interactive)
+
+const port = process.env.PORT || 4413;
+app.listen(port, function() {
+    console.log(`Server is live on ${port}`)
+});
